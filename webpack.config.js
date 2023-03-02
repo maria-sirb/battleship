d187afb3fc123a7d04580f3fc1caa6f600c1cb88
maria-sirb <sirbmariafelicia@gmail.com>
2023-03-02 17:25:06 +0200

    add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      }
     ],
   },
  };
